70812c9a9ea80cbfea519ab4d7c9f9f11999ba02
= <=>
2023-09-14 20:18:18 -0500

    creando proyecto

---
+++ LAB5-20200825-GTICS/src/main/java/com/example/lab520200825gtics/Lab520200825GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab520200825gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520200825GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520200825GticsApplication.class, args);
+    }
+
+}
---
+++ LAB5-20200825-GTICS/src/test/java/com/example/lab520200825gtics/Lab520200825GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab520200825gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520200825GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
